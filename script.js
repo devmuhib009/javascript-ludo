480fee823bf4a3e0dcda3dc61a5a978b4a47dac1
devmuhib009 <142894870+devmuhib009@users.noreply.github.com>
2023-11-22 18:26:15 +0600

    Ludo Game Building

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+// Starting conditions
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+current0El.textContent = 0;
+current1El.textContent = 0;
+
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // Generating random numbers
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to next player
+    //switchPlayer();
+  }
+});
